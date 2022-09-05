65509f21648088e24124f2698578c53af3b86379
Emilio Maccari <emilio@maccari.com.br>
2022-09-05 15:50:51 -0300

    firts

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { Home } from './src/pages/Home';
+
+
+export default function App() {
+  return (
+    <View style= {styles.container}>
+      <StatusBar style="auto" />
+      <Home/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+});
